e863d7df8a1204360e9f3ce884f2ddf1e23c5c2d
0xSunill <sunday7637@gmail.com>
2025-05-04 14:38:37 +0000

    add nav bar

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 
+import Nav from "@/components/Nav";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
-      hello sunil
+      <Nav />
     </div>
   );
 }
---
+++ components/Nav.jsx
@@ -0,0 +1,18 @@
+"use client"
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div className="bg-[#0D1117] border-b w-full h-20 px-4 md:px-10 flex justify-between items-center">
+            <div className="text-white text-[20px] md:text-3xl font-semibold">
+                Sun Launcher
+            </div>
+            <div className="flex-shrink-0">
+                <WalletMultiButton />
+            </div>
+        </div>
+    )
+}
+
+export default Nav
