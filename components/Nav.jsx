ec1cbb7d64a6187bc98ba36789712ecc73396bb1
0xSunill <sunday7637@gmail.com>
2025-05-04 16:01:36 +0000

    add nav logo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,23 @@
 "use client"
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Nav = () => {
+    const router = useRouter()
     return (
         <div className="bg-[#0D1117] border-b w-full h-20 px-4 md:px-20 flex justify-between items-center">
-            <div className="text-white text-[20px] md:text-3xl font-semibold">
+            <div className="text-white flex justify-center items-center text-[20px] md:text-3xl font-semibold">
+                <div onClick={() => router.push('/')}>
+                    <Image
+                        src="/Images/logo.jpg"
+                        alt="logo"
+                        width={50}
+                        height={50}
+                        className="rounded-full object-cover aspect-square mr-2 "
+                    />
+                </div>
                 Sun Launcher
             </div>
             <div className="flex-shrink-0">
