06c3628944d258fe52a9b43e3e6fcaeaf60978ef
0xSunill <sunday7637@gmail.com>
2025-05-04 14:26:48 +0000

    add wallet adapter

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
       hello sunil
     </div>
   );
