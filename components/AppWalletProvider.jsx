eeacd172d73f2b656d18fcbfcdeca5f67a24f5e5
0xSunill <sunday7637@gmail.com>
2025-05-06 15:51:03 +0000

    fix socials

--- components/AppWalletProvider.jsx
+++ components/AppWalletProvider.jsx
@@ -8,9 +8,7 @@ import {
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletDisconnectButton, WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
-// import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
 
-// Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 import {
@@ -19,7 +17,7 @@ import {
   TorusWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 
-// imports here
+
 
 export default function AppWalletProvider({ children }) {
     const network = WalletAdapterNetwork.Devnet;
