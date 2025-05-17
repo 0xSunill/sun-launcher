d7b155cd96e135f7c07d7ef8080100358859a2cb
0xSunill <sunday7637@gmail.com>
2025-05-17 17:17:06 +0000

    addded mint button

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -15,17 +15,27 @@ const Hero = ({ setLoading }) => {
         }, 1000)
     }
 
-  
+
+    const handleClickMint = () => {
+        console.log("mint token route")
+    }
     return (
         <div className='flex flex-1 py-16 px-4 md:px-20 pt-20 md:flex-row flex-col '>
             <div className='flex-1 flex  justify-center items-start flex-col gap-2  '>
                 <h1 className='text-6xl font-bold text-white'>Create Your Solana <br />token</h1>
                 <p className='text-lg text-gray-400 mt-4 mb-2 text-wrap max-w-[470px] '> Launch your own SPL token on the Solana blockchain in just a few clicks. No coding required. Fast, secure, and fully decentralized.</p>
-                <button
-                    onClick={handleClick}
-                    className="bg-purple-600 hover:bg-purple-700 text-white font-semibold py-3 px-6 rounded-xl shadow-md transition duration-300 ease-in-out">
-                    Create Token
-                </button>
+                <div className='flex flex-row justify-start  items-center gap-4'>
+                    <button
+                        onClick={handleClick}
+                        className="bg-purple-600  hover:bg-purple-700 text-white font-semibold py-3 px-6 rounded-xl shadow-md transition duration-300 ease-in-out">
+                        Create Token
+                    </button>
+                    <button
+                        onClick={handleClickMint}
+                        className="bg-green-600 sm:ml-6 hover:bg-green-700 text-white font-semibold py-3 px-6 rounded-xl shadow-md transition duration-300 ease-in-out">
+                        Create Token
+                    </button>
+                </div>
             </div>
             <div className=''>
                 <Image
